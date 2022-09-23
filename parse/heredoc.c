41fe4f34a1915e9dd3a2e4cb2f0cbdea99b50d54
Sara Jo <sjo@c7r1s5.42seoul.kr>
2022-09-23 14:48:01 +0900

    heredoc.c 파일 노미넷 맞춤

--- parse/heredoc.c
+++ parse/heredoc.c
@@ -6,53 +6,53 @@
 /*   By: sjo <sjo@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 03:33:47 by sjo               #+#    #+#             */
-/*   Updated: 2022/09/21 05:00:01 by sjo              ###   ########.fr       */
+/*   Updated: 2022/09/23 14:47:43 by sjo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void remove_temp_file(void)
+void	remove_temp_file(void)
 {
-    struct stat s;
+	struct stat	s;
 
-    if (stat("sjo.sumjang", &s) == 0)
-        unlink("sjo.sumjang");
+	if (stat("sjo.sumjang", &s) == 0)
+		unlink("sjo.sumjang");
 }
 
-static void finish_heredoc(char **line, int fd, int end_status)
+void	finish_heredoc(char **line, int fd, int end_status)
 {
-    if (*line != NULL)
-        free(*line);
-    if (fd >= 0)
-        close(fd);
-    exit(end_status);
+	if (*line != NULL)
+		free(*line);
+	if (fd >= 0)
+		close(fd);
+	exit(end_status);
 }
 
-int heredoc_child(char *delimiter)
+int	heredoc_child(char *delimiter)
 {
-    int fd;
-    char *line;
+	int		fd;
+	char	*line;
 
-    set_heredoc_signal();
-    line = NULL;
-    fd = open("sjo.sumjang", O_RDWR | O_CREAT | O_TRUNC, 0666);
-    if (fd < 0)
-    {
-        parse_error(5);
-        finish_heredoc(&line, fd, 1);
-    }
-    while (1)
-    {
-        line = readline("> ");
-        if (line == NULL)
-            move_heredoc_curser(fd);
-        if (ft_strcmp(line, delimiter) == 0)
-            finish_heredoc(&line, fd, 0);
-        if (write_heredoc(fd, line) == 0)
-            finish_heredoc(&line, fd, 1);
-        free(line);
-    }
-    line = NULL;
-    finish_heredoc(&line, fd, 0);
+	set_heredoc_signal();
+	line = NULL;
+	fd = open("sjo.sumjang", O_RDWR | O_CREAT | O_TRUNC, 0666);
+	if (fd < 0)
+	{
+		parse_error(5);
+		finish_heredoc(&line, fd, 1);
+	}
+	while (1)
+	{
+		line = readline("> ");
+		if (line == NULL)
+			move_heredoc_curser(fd);
+		if (ft_strcmp(line, delimiter) == 0)
+			finish_heredoc(&line, fd, 0);
+		if (write_heredoc(fd, line) == 0)
+			finish_heredoc(&line, fd, 1);
+		free(line);
+	}
+	line = NULL;
+	finish_heredoc(&line, fd, 0);
 }
